ea2752b60fbfa8c00988dcd696dda38f89f48beb
Michael Reis <michael_farias14@hotmail.com>
2024-12-05 07:05:28 -0300

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import path from 'path';
+
+const nextConfig = {
+    webpack(config, { isServer }) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+            };
+        }
+
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve('src'),
+        };
+
+        return config;
+    },
+    reactStrictMode: false,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 export default nextConfig;
